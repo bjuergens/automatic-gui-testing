Added coverage calculation, fixed clicking
Coverage increase is now calculated after every click.
Fixed SettingsDialog always staying on top
Fixed clicks also working in SettingsDialog

envs/gui_env/gui_env.py
@@ -1,3 +1,4 @@
+import logging
 import multiprocessing as mp
 from multiprocessing import Process, Pipe
 from multiprocessing.connection import Connection
@@ -41,7 +42,6 @@ class RegisterClickThread(QThread):
                     return
                 finally:
                     if isinstance(received_data, Tuple):
-                        print("Emitting position!")
                         self.position_signal.emit(received_data[0], received_data[1])
                     elif isinstance(received_data, bool):
                         print("Choosing random widget!")
@@ -75,7 +75,7 @@ class GUIEnv(gym.Env):
     def _on_timeout(self):
         # Initial observation trigger
         screenshot = self.main_window.take_screenshot()
-        self.main_window.screenshot_signal.emit(screenshot)
+        self.main_window.screenshot_signal.emit(screenshot, 0)
 
     def _start_application(self, click_connection_child: Connection, terminate_connection_child: Connection):
         app = QApplication()
@@ -95,10 +95,9 @@ class GUIEnv(gym.Env):
 
         app.exec()
 
-    @Slot(np.ndarray)
-    def _get_observation(self, observation: np.ndarray):
-        print("Got observation")
-        self.click_connection_child.send(observation)
+    @Slot(np.ndarray, float)
+    def _get_observation(self, observation: np.ndarray, reward: float):
+        self.click_connection_child.send((observation, reward))
 
     @Slot(np.ndarray, int, int)
     def _get_observation_random_widget(self, observation: np.ndarray, pos_x: int, pos_y: int):
@@ -106,13 +105,17 @@ class GUIEnv(gym.Env):
         self.click_connection_child.send((observation, pos_x, pos_y))
 
     def step(self, action: Union[Tuple[int, int], bool]) -> np.ndarray:
+        logging.debug("Sending Action")
         self.click_connection_parent.send(action)
         observation = self.click_connection_parent.recv()
+        logging.debug("Received observation")
 
         if isinstance(action, bool):
             observation, pos_x, pos_y = observation
+        else:
+            observation, reward = observation
 
-        return observation
+        return reward, observation
 
     def reset(self):
         # TODO Cleanup needed to restart an env that previously ran; similar to the code in close(), but calling close

envs/gui_env/src/main_window.py
@@ -1,14 +1,17 @@
 import logging
+import os
 import random
 import sys
 from functools import partial
 
+import coverage.exceptions
 import numpy as np
 from PySide6.QtCore import Qt, QPoint, Slot, Signal
 from PySide6.QtGui import QAction, QPalette, QPaintEvent, QMouseEvent, QColor, QFont, QFontDatabase
 from PySide6.QtGui import QPainter, QPen, QBrush, QColorConstants
 from PySide6.QtTest import QTest
-from PySide6.QtWidgets import QApplication, QMainWindow, QMenuBar
+from PySide6.QtWidgets import QApplication, QMainWindow, QMenuBar, QWidget
+from coverage import Coverage
 
 from envs.gui_env.src.backend.calculator import Calculator
 from envs.gui_env.src.backend.figure_printer import FigurePrinter
@@ -17,20 +20,23 @@ from envs.gui_env.src.settings_dialog import SettingsDialog
 from envs.gui_env.src.utils.utils import load_ui, convert_qimage_to_ndarray
 
 
+WINDOW_SIZE = (448, 448)
+
+
 class MainWindow(QMainWindow):
-    screenshot_signal = Signal(np.ndarray)
+    screenshot_signal = Signal(np.ndarray, float)
     screenshot_and_coordinates_signal = Signal(np.ndarray, int, int)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
         self.setWindowTitle("test-gui-worldmodels")
-        self.setFixedSize(600, 600)
+        self.setFixedSize(*WINDOW_SIZE)
 
         self.main_window = load_ui("envs/gui_env/src/main_window.ui")
 
         # Initialize menu bar
-        self.menu_bar = QMenuBar()
+        self.menu_bar = QMenuBar(parent=self)
         self.settings_action = QAction("Settings")
         self.menu_bar.addAction(self.settings_action)
         self.setMenuBar(self.menu_bar)
@@ -56,11 +62,18 @@ class MainWindow(QMainWindow):
 
         self.settings_dialog = SettingsDialog(text_printer=self.text_printer, calculator=self.calculator,
                                               figure_printer=self.figure_printer, parent=self)
-        self.settings_action.triggered.connect(self.settings_dialog.exec)
+        # Disables possible clicks outside the dialog, and keeps the dialog always on top until it is closed
+        self.setWindowModality(Qt.ApplicationModal)
+        self.settings_dialog.setWindowFlags(Qt.Dialog | Qt.WindowStaysOnTopHint | Qt.X11BypassWindowManagerHint)
+
+        self.settings_action.triggered.connect(self.settings_dialog.open)
 
         self.settings_dialog.figure_printer_activated.connect(self._toogle_figure_printing)
 
         self.setCentralWidget(self.main_window)
+
+        self.current_coverage = Coverage()
+        self.old_coverage_percentage = self.get_current_coverage_percentage()
 
     def _connect_buttons(self):
         # Text Printer
@@ -130,18 +143,45 @@ class MainWindow(QMainWindow):
 
         return convert_qimage_to_ndarray(screenshot)
 
+    def get_current_coverage_percentage(self):
+        with open(os.devnull, "w") as f:
+            try:
+                coverage_percentage = self.current_coverage.report(file=f)
+            except coverage.exceptions.CoverageException:
+                # Is thrown when nothing was ever recorded by the coverage object
+                coverage_percentage = 0
+        return coverage_percentage
+
+    def calculate_coverage_increase(self):
+        new_coverage_percentage = self.get_current_coverage_percentage()
+        reward = new_coverage_percentage - self.old_coverage_percentage
+        self.old_coverage_percentage = new_coverage_percentage
+        return reward
+
+    def execute_mouse_click(self, recv_widget: QWidget, local_pos: QPoint) -> float:
+        self.current_coverage.start()
+        QTest.mouseClick(recv_widget, Qt.LeftButton, Qt.NoModifier, local_pos)
+        self.current_coverage.stop()
+
+        reward = self.calculate_coverage_increase()
+
+        return reward
+
     @Slot(int, int)
     def simulate_click(self, pos_x: int, pos_y: int):
         pos = QPoint(pos_x, pos_y)
-        recv_widget = self.childAt(pos)
-        local_pos = recv_widget.mapFrom(self.main_window, pos)
+        global_pos = self.mapToGlobal(pos)
+        logging.debug(f"Received position {pos}, mapped to global position {global_pos}")
 
-        QTest.mouseClick(recv_widget, Qt.LeftButton, Qt.NoModifier, local_pos)
+        recv_widget = QApplication.widgetAt(global_pos)
+        local_pos = recv_widget.mapFromGlobal(global_pos)
 
-        logging.debug(f"Received pos {pos_x, pos_y}, clicked widget '{recv_widget}' at local pos '{local_pos}'")
+        logging.debug(f"Found widget {recv_widget}, mapped to local position {local_pos}")
+
+        reward = self.execute_mouse_click(recv_widget, local_pos)
 
         screenshot = self.take_screenshot()
-        self.screenshot_signal.emit(screenshot)
+        self.screenshot_signal.emit(screenshot, reward)
 
     @Slot()
     def simulate_click_on_random_widget(self):
@@ -155,7 +195,7 @@ class MainWindow(QMainWindow):
 
         click_point = QPoint(x, y)
 
-        QTest.mouseClick(randomly_selected_widget, Qt.LeftButton, Qt.NoModifier, click_point)
+        reward = self.execute_mouse_click(randomly_selected_widget, click_point)
 
         # TODO this will probably fail in the settings dialog as this prints out a coordinate system in the dialog's
         #  coordinate system but we want the coordinate of the window