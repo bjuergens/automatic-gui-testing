f5eece1c039b74ecce95cd5a099923c48fbf41f4
Patrick Deubel <patrick.deubel@gmail.com>
2021-12-09 13:36:54 +0100

    Fixed combo boxes in figure printer that exceeded window size
    Also decreased font size by one point in figure printer

--- envs/gui_env/src/main_window.py
+++ envs/gui_env/src/main_window.py
@@ -94,7 +94,7 @@ class MainWindow(QMainWindow):
 
         # Need a monospace font to display the ASCII art correctly
         document = self.main_window.figure_printer_output.document()
-        font = QFontDatabase.font("Bitstream Vera Sans Mono", "Normal", 7)
+        font = QFontDatabase.font("Bitstream Vera Sans Mono", "Normal", 6)
         document.setDefaultFont(font)
 
     def _connect_buttons(self):
