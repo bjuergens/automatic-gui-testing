17c02c40ffe21b882a7c1405b75f7701944c5377
Patrick Deubel <patrick.deubel@gmail.com>
2021-12-21 17:41:17 +0100

    Train VAE: Added logging, changed Loss function significantly
    - Changed Loss function to use also KLD Weight and KLD Warm Up
    - Changed computation of KLD a bit -> Takes the mean over the batch dimension after
    calculating the sum. I think this is mathematically more correct as the previous version
    (the previous version took the sum over everything and reduced it, where now the sum is taken
    only over the latent dimension and then the mean over the batch dimension, looking at Appendix B
    of the Kingma VAE paper this seems mathematically more sound I guess)

--- train_vae.py
+++ train_vae.py
@@ -1,4 +1,3 @@
-# import argparse
 import os
 
 import click
@@ -19,23 +18,51 @@ from utils.misc import save_checkpoint
 # WARNING : THIS SHOULD BE REPLACE WITH PYTORCH 0.5
 # from utils.learning import EarlyStopping
 # from utils.learning import ReduceLROnPlateau
-from data.loaders import RolloutObservationDataset
+# from data.loaders import RolloutObservationDataset
 
 
-# Reconstruction + KL divergence losses summed over all elements and batch
-def loss_function(recon_x, x, mu, logsigma):
-    """ VAE loss function """
-    BCE = F.mse_loss(recon_x, x, size_average=False)
+def loss_function(experiment: Experiment, x: torch.Tensor, reconstruction_x: torch.Tensor, mu: torch.Tensor,
+                  log_var: torch.Tensor, kld_weight: float, current_epoch: int, max_epochs: int,
+                  is_train: bool = True) -> torch.Tensor:
+    # MSE
+    batch_dim = x.size(0)
+    reconstruction_loss = F.mse_loss(x, reconstruction_x, reduction="sum") / batch_dim
+
+    # KLD
+    kld_warmup_term = current_epoch / max_epochs
 
     # see Appendix B from VAE paper:
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
     # https://arxiv.org/abs/1312.6114
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    KLD = -0.5 * torch.sum(1 + 2 * logsigma - mu.pow(2) - (2 * logsigma).exp())
-    return BCE + KLD
+    # Take also the mean over the batch_dim (outermost function call)
+    kld_loss = torch.mean(-0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp(), dim=1), dim=0)
+
+    loss = reconstruction_loss + kld_weight * kld_warmup_term * kld_loss
+
+    if is_train:
+        experiment.log({
+            "loss": loss,
+            "reconstruction_loss": reconstruction_loss,
+            "kld": kld_loss,
+            "mu": torch.mean(mu),
+            "log_var": torch.mean(log_var),
+            "var": torch.mean(log_var.exp())
+        })
+    else:
+        experiment.log({
+            "val_loss": loss,
+            "val_reconstruction_loss": reconstruction_loss,
+            "val_kld": kld_loss,
+            "val_mu": torch.mean(mu),
+            "val_log_var": torch.mean(log_var),
+            "val_var": torch.mean(log_var.exp())
+        })
+
+    return loss
 
 
-def train(epoch, model, train_loader, device, optimizer):
+def train(model, experiment, train_loader, optimizer, device, current_epoch, max_epochs, kld_weight):
     """ One training epoch """
     model.train()
     # dataset_train.load_next_buffer()
@@ -44,33 +71,44 @@ def train(epoch, model, train_loader, device, optimizer):
         data = data.to(device)
         optimizer.zero_grad()
         recon_batch, mu, logvar = model(data)
-        loss = loss_function(recon_batch, data, mu, logvar)
+        loss = loss_function(experiment, data, recon_batch, mu, logvar, kld_weight, current_epoch, max_epochs)
         loss.backward()
         train_loss += loss.item()
         optimizer.step()
         if batch_idx % 20 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
+                current_epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader),
                 loss.item() / len(data)))
 
     print('====> Epoch: {} Average loss: {:.4f}'.format(
-        epoch, train_loss / len(train_loader.dataset)))
+        current_epoch, train_loss / len(train_loader.dataset)))
 
 
-def validate(model, val_loader, device):
+def validate(model, experiment: Experiment, val_loader, device, current_epoch, max_epochs, kld_weight):
     """ One test epoch """
     model.eval()
-    # dataset_test.load_next_buffer()
+
     test_loss = 0
+
+    logged_one_batch = False
+
     with torch.no_grad():
         for data in val_loader:
             data = data.to(device)
-            recon_batch, mu, logvar = model(data)
-            test_loss += loss_function(recon_batch, data, mu, logvar).item()
+            recon_batch, mu, log_var = model(data)
+            test_loss += loss_function(experiment, data, recon_batch, mu, log_var, kld_weight, current_epoch, max_epochs).item()
+
+            if not logged_one_batch and not experiment.debug:
+                experiment.add_images("originals", data, global_step=current_epoch)
+                experiment.add_images("reconstructions", recon_batch, global_step=current_epoch)
+                logged_one_batch = True
 
     test_loss /= len(val_loader.dataset)
     print('====> Test set loss: {:.4f}'.format(test_loss))
+
+    model.train()
+
     return test_loss
 
 
@@ -78,24 +116,11 @@ def validate(model, val_loader, device):
 @click.option("-c", "--config", "config_path", type=str, required=True,
               help="Path to a YAML configuration containing training options")
 def main(config_path: str):
-    # parser = argparse.ArgumentParser(description='Train the VAE (V model of the world model)')
-    # parser.add_argument('--batch-size', type=int, default=32, metavar='N',
-    #                     help='Batch size for training (default: 32)')
-    # parser.add_argument('--epochs', type=int, default=1000, metavar='N',
-    #                     help='Number of epochs to train (default: 1000)')
-    # parser.add_argument('--logdir', type=str, help='Directory where results are logged')
-    # parser.add_argument('--noreload', action='store_true',
-    #                     help='Best model is not reloaded if specified')
-    # parser.add_argument('--nosamples', action='store_true',
-    #                     help='Does not save samples during training if specified')
-
     with open(config_path, "r") as file:
         config = yaml.safe_load(file)
 
-    # SIZE_HEIGHT, SIZE_WIDTH = 224, 224
-    # LATENT_SIZE = 64
+    kld_weight = config["experiment_parameters"]["kld_weight"]
 
-    # args = parser.parse_args()
     torch.manual_seed(123)
 
     # Fix numeric divergence due to bug in Cudnn
@@ -131,19 +156,6 @@ def main(config_path: str):
     else:
         raise RuntimeError("Currently only 'gui-dataset' supported as the dataset")
 
-    # transform_test = transforms.Compose([
-    #     transforms.ToPILImage(),
-    #     transforms.Resize((RED_SIZE, RED_SIZE)),
-    #     transforms.ToTensor(),
-    # ])
-
-
-    #
-    # dataset_train = RolloutObservationDataset('datasets/carracing',
-    #                                           transform_train, train=True)
-    # dataset_test = RolloutObservationDataset('datasets/carracing',
-    #                                          transform_test, train=False)
-
     additional_dataloader_args = {'num_workers': config["trainer_parameters"]["num_workers"], 'pin_memory': True}
     batch_size = config["experiment_parameters"]["batch_size"]
 
@@ -172,12 +184,12 @@ def main(config_path: str):
     save_dir = os.path.join(config["logging_parameters"]["save_dir"], config["experiment_parameters"]["dataset"])
     os.makedirs(save_dir, exist_ok=True)
 
-    # TODO figure out if autosave is needed
     experiment = Experiment(
         save_dir=save_dir,
         name=config["model_parameters"]["name"],
         debug=config["logging_parameters"]["debug"],  # Turns off logging if True
-        create_git_tag=False
+        create_git_tag=False,
+        autosave=True
     )
 
     # vae_dir = join(args.logdir, 'vae')
@@ -198,38 +210,39 @@ def main(config_path: str):
     #     earlystopping.load_state_dict(state['earlystopping'])
 
     current_best = None
-    epochs = config["trainer_parameters"]["max_epochs"]
+    max_epochs = config["trainer_parameters"]["max_epochs"]
 
-    log_dir = experiment.get_logdir().split("tf")[0]
+    if not experiment.debug:
+        log_dir = experiment.get_logdir().split("tf")[0]
+        os.makedirs(os.path.join(log_dir, "samples"), exist_ok=True)
 
-    os.makedirs(os.path.join(log_dir, "samples"), exist_ok=True)
-
-    for epoch in range(1, epochs + 1):
-        train(epoch, model, train_loader, device, optimizer)
-        validation_loss = validate(model, val_loader, device)
+    for current_epoch in range(0, max_epochs):
+        train(model, experiment, train_loader, optimizer, device, current_epoch, max_epochs, kld_weight)
+        validation_loss = validate(model, experiment, val_loader, device, current_epoch, max_epochs, kld_weight)
         # scheduler.step(test_loss)
         # earlystopping.step(test_loss)
 
         # checkpointing
-        best_filename = os.path.join(log_dir, 'best.tar')
-        filename = os.path.join(log_dir, 'checkpoint.tar')
-        is_best = not current_best or validation_loss < current_best
-        if is_best:
-            current_best = validation_loss
+        if not experiment.debug:
+            best_filename = os.path.join(log_dir, 'best.tar')
+            filename = os.path.join(log_dir, 'checkpoint.tar')
+            is_best = not current_best or validation_loss < current_best
+            if is_best:
+                current_best = validation_loss
 
-        save_checkpoint({
-            'epoch': epoch,
-            'state_dict': model.state_dict(),
-            'precision': validation_loss,
-            'optimizer': optimizer.state_dict()
-            # 'scheduler': scheduler.state_dict(),
-            # 'earlystopping': earlystopping.state_dict()
-        }, is_best, filename, best_filename)
+            save_checkpoint({
+                'epoch': current_epoch,
+                'state_dict': model.state_dict(),
+                'precision': validation_loss,
+                'optimizer': optimizer.state_dict()
+                # 'scheduler': scheduler.state_dict(),
+                # 'earlystopping': earlystopping.state_dict()
+            }, is_best, filename, best_filename)
 
-        with torch.no_grad():
-            sample = torch.randn(10, config["model_parameters"]["latent_size"]).to(device)
-            reconstruction = model.decoder(sample).cpu()
-            save_image(reconstruction, os.path.join(log_dir, 'samples/sample_' + str(epoch) + '.png'))
+            with torch.no_grad():
+                sample = torch.randn(10, config["model_parameters"]["latent_size"]).to(device)
+                reconstruction = model.decoder(sample).cpu()
+                save_image(reconstruction, os.path.join(log_dir, 'samples/sample_' + str(current_epoch) + '.png'))
 
         # if earlystopping.stop:
         #     print("End of Training because of early stopping at epoch {}".format(epoch))
