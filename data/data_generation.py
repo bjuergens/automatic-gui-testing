Adapted data generation to store rewards and actions in 32bit precision Fixed a bug in dataset where length could be negative or zero

data/data_generation.py
@@ -87,7 +87,11 @@ def start_monkey_tester(env: gym.Env, stop_mode: str, amount: int, chosen_direct
     else:
         reward_sum, rewards, actions = _iteration_mode_rollout(amount, env, observations_directory)
 
-    np.savez(os.path.join(chosen_directory, "data.npz"), rewards=rewards, actions=actions)
+    np.savez(
+        os.path.join(chosen_directory, "data.npz"),
+        rewards=np.array(rewards, dtype=np.float32),
+        actions=np.array(actions, dtype=np.int32)
+    )
 
     logging.info(f"Finished data generation with a summed up reward of {reward_sum}")
     env.close()

data/gui_dataset.py
@@ -14,8 +14,8 @@ class GUISequenceDataset(Dataset):
         self.transform = transform
 
         data = np.load(os.path.join(self.root_dir, "data.npz"))
-        self.rewards: np.ndarray = data["rewards"]
-        self.actions: np.ndarray = data["actions"]
+        self.rewards: torch.Tensor = torch.from_numpy(data["rewards"])
+        self.actions: torch.Tensor = torch.from_numpy(data["actions"])
 
         self.observation_files = [
             os.path.join(self.root_dir, "observations", img_file)
@@ -23,7 +23,9 @@ class GUISequenceDataset(Dataset):
         ]
         self.observation_files.sort()
 
-        assert self.rewards.shape[0] == self.actions.shape[0] == (len(self.observation_files) - 1)
+        assert self.rewards.size(0) == self.actions.size(0) == (len(self.observation_files) - 1)
+        assert self.__len__() > 0, ("Dataset length is 0 or negative, probably too large sequence length or too few "
+                                    "data samples")
 
         split_percentage = 0.05
         split_index = round(len(self.rewards) * (1 - split_percentage))
@@ -37,7 +39,7 @@ class GUISequenceDataset(Dataset):
             self.actions = self.actions[split_index:]
 
     def __len__(self):
-        return self.actions.shape[0] - self.sequence_length - 1
+        return self.rewards.size(0) - self.sequence_length - 1
 
     def __getitem__(self, index):
         all_observations = []