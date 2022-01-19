b62371677f76b2dbf257681da67d261b955bbc7f
Patrick Deubel <patrick.deubel@gmail.com>
2022-01-19 16:01:34 +0100

    BaseRNN: Changed function to 'predict'
    - Outputs predicted next latent and reward

--- models/rnn/base_rnn.py
+++ models/rnn/base_rnn.py
@@ -46,7 +46,7 @@ class BaseRNN(abc.ABC, nn.Module):
         return outputs, (self.hidden_state, self.cell_state)
 
     @abc.abstractmethod
-    def predict_next_latent(self, model_output, latents):
+    def predict(self, model_output, latents) -> Tuple[torch.Tensor, torch.Tensor]:
         pass
 
     @abc.abstractmethod
@@ -65,10 +65,11 @@ class BaseMDNRNN(BaseRNN):
 
         self.number_of_gaussians = model_parameters["number_of_gaussians"]
 
-    def predict_next_latent(self, model_output, latents):
+    def predict(self, model_output, latents):
         mus = model_output[0]
         sigmas = model_output[1]
         log_pi = model_output[2]
+        rewards = model_output[3]
 
         # batch: (BATCH_SIZE, SEQ_LEN, 1, L_SIZE)
         # mus, sigmas: (BATCH_SIZE, SEQ_LEN, N_GAUSS, L_SIZE)
@@ -79,10 +80,10 @@ class BaseMDNRNN(BaseRNN):
         # log_pi: (BATCH_SIZE, SEQ_LEN, N_GAUSS, 1)
         pi = log_pi.exp().unsqueeze(-1)
 
-        prediction = torch.sum(prob * pi, dim=2)
+        latent_prediction = torch.sum(prob * pi, dim=2)
 
         # Result: (BATCH_SIZE, SEQ_LEN, L_SIZE)
-        return prediction
+        return latent_prediction, rewards
 
     def forward(self, latents: torch.Tensor, actions: torch.Tensor):
         sequence_length = latents.size(1)
@@ -151,11 +152,11 @@ class BaseSimpleRNN(BaseRNN):
     def __init__(self, model_parameters: dict, latent_size: int, batch_size: int, device: torch.device):
         super().__init__(model_parameters, latent_size, batch_size, device)
 
-    def predict_next_latent(self, model_output, latents=None):
+    def predict(self, model_output, latents=None):
         # This function mostly exists for mixture density network as the actual calculation of the next latent state
         # is not required for training, just the calculation of the predicted probability distribution.
         # But since we want to use the same interface, just return the prediction here
-        return model_output[0]
+        return model_output[0], model_output[1]
 
     def forward(self, latents: torch.Tensor, actions: torch.Tensor):
         outputs, _ = self.rnn_forward(latents, actions)
