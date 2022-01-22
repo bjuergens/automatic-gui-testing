b38ff2a3fc5a62bbf9fa552268fb7c46bec2690b
Patrick Deubel <patrick.deubel@gmail.com>
2022-01-22 15:53:17 +0100

    SequenceBatchSampler: Added __len__ function

--- data/dataset_implementations/rnn/sequence_batch_sampler.py
+++ data/dataset_implementations/rnn/sequence_batch_sampler.py
@@ -34,3 +34,9 @@ class GUISequenceBatchSampler(Sampler):
         # here for future reference and clarity.
         if len(batch) > 0 and not self.drop_last:
             yield batch
+
+    def __len__(self) -> int:
+        if self.drop_last:
+            return len(self.sampler) // self.batch_size
+        else:
+            return (len(self.sampler) + self.batch_size - 1) // self.batch_size
