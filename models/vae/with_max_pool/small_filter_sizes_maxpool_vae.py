df8644b44868e9f698d0a1cfe4d50b9aa6c17640
Patrick Deubel <patrick.deubel@gmail.com>
2022-01-28 14:59:49 +0100

    Added VAE architecture with small kernel sizes and max pooling

--- models/vae/with_max_pool/small_filter_sizes_maxpool_vae.py
+++ models/vae/with_max_pool/small_filter_sizes_maxpool_vae.py
@@ -6,19 +6,19 @@ from torch import nn
 from models.vae import BaseVAE
 
 
-class LargeFilterSizesMaxPoolVAE(BaseVAE):
+class SmallFilterSizesMaxPoolVAE(BaseVAE):
 
     def __init__(self, model_parameters: dict):
         super().__init__(model_parameters)
 
         assert len(self.hidden_dimensions) == 6, "For the chosen VAE architecture, 6 hidden dimensions are needed"
 
-        conv_1 = nn.Conv2d(in_channels=self.input_channels, out_channels=self.hidden_dimensions[0], kernel_size=11, stride=1, padding=5)
-        conv_2 = nn.Conv2d(in_channels=self.hidden_dimensions[0], out_channels=self.hidden_dimensions[1], kernel_size=11, stride=1, padding=5)
-        conv_3 = nn.Conv2d(in_channels=self.hidden_dimensions[1], out_channels=self.hidden_dimensions[2], kernel_size=11, stride=1, padding=5)
-        conv_4 = nn.Conv2d(in_channels=self.hidden_dimensions[2], out_channels=self.hidden_dimensions[3], kernel_size=11, stride=1, padding=5)
-        conv_5 = nn.Conv2d(in_channels=self.hidden_dimensions[3], out_channels=self.hidden_dimensions[4], kernel_size=11, stride=1, padding=5)
-        conv_6 = nn.Conv2d(in_channels=self.hidden_dimensions[4], out_channels=self.hidden_dimensions[5], kernel_size=11, stride=1, padding=5)
+        conv_1 = nn.Conv2d(in_channels=self.input_channels, out_channels=self.hidden_dimensions[0], kernel_size=3, stride=1, padding=1)
+        conv_2 = nn.Conv2d(in_channels=self.hidden_dimensions[0], out_channels=self.hidden_dimensions[1], kernel_size=3, stride=1, padding=1)
+        conv_3 = nn.Conv2d(in_channels=self.hidden_dimensions[1], out_channels=self.hidden_dimensions[2], kernel_size=3, stride=1, padding=1)
+        conv_4 = nn.Conv2d(in_channels=self.hidden_dimensions[2], out_channels=self.hidden_dimensions[3], kernel_size=3, stride=1, padding=1)
+        conv_5 = nn.Conv2d(in_channels=self.hidden_dimensions[3], out_channels=self.hidden_dimensions[4], kernel_size=3, stride=1, padding=1)
+        conv_6 = nn.Conv2d(in_channels=self.hidden_dimensions[4], out_channels=self.hidden_dimensions[5], kernel_size=3, stride=1, padding=1)
 
         if self.use_batch_norm:
             self.conv_layer_1 = nn.Sequential(conv_1, nn.BatchNorm2d(self.hidden_dimensions[0]), self.activation_function(), nn.MaxPool2d(2, 2))
@@ -41,12 +41,12 @@ class LargeFilterSizesMaxPoolVAE(BaseVAE):
 
         self.fc_decoder = nn.Linear(self.latent_size, 6 * 6 * self.hidden_dimensions[5])
 
-        transposed_conv_1 = nn.ConvTranspose2d(in_channels=self.hidden_dimensions[5], out_channels=self.hidden_dimensions[4], kernel_size=11, stride=2, padding=4, output_padding=1)
-        transposed_conv_2 = nn.ConvTranspose2d(in_channels=self.hidden_dimensions[4], out_channels=self.hidden_dimensions[3], kernel_size=11, stride=2, padding=5, output_padding=1)
-        transposed_conv_3 = nn.ConvTranspose2d(in_channels=self.hidden_dimensions[3], out_channels=self.hidden_dimensions[2], kernel_size=11, stride=2, padding=5, output_padding=1)
-        transposed_conv_4 = nn.ConvTranspose2d(in_channels=self.hidden_dimensions[2], out_channels=self.hidden_dimensions[1], kernel_size=11, stride=2, padding=5, output_padding=1)
-        transposed_conv_5 = nn.ConvTranspose2d(in_channels=self.hidden_dimensions[1], out_channels=self.hidden_dimensions[0], kernel_size=11, stride=2, padding=5, output_padding=1)
-        transposed_conv_6 = nn.ConvTranspose2d(in_channels=self.hidden_dimensions[0], out_channels=self.input_channels, kernel_size=11, stride=2, padding=5, output_padding=1)
+        transposed_conv_1 = nn.ConvTranspose2d(in_channels=self.hidden_dimensions[5], out_channels=self.hidden_dimensions[4], kernel_size=3, stride=2, padding=0, output_padding=1)
+        transposed_conv_2 = nn.ConvTranspose2d(in_channels=self.hidden_dimensions[4], out_channels=self.hidden_dimensions[3], kernel_size=3, stride=2, padding=1, output_padding=1)
+        transposed_conv_3 = nn.ConvTranspose2d(in_channels=self.hidden_dimensions[3], out_channels=self.hidden_dimensions[2], kernel_size=3, stride=2, padding=1, output_padding=1)
+        transposed_conv_4 = nn.ConvTranspose2d(in_channels=self.hidden_dimensions[2], out_channels=self.hidden_dimensions[1], kernel_size=3, stride=2, padding=1, output_padding=1)
+        transposed_conv_5 = nn.ConvTranspose2d(in_channels=self.hidden_dimensions[1], out_channels=self.hidden_dimensions[0], kernel_size=3, stride=2, padding=1, output_padding=1)
+        transposed_conv_6 = nn.ConvTranspose2d(in_channels=self.hidden_dimensions[0], out_channels=self.input_channels, kernel_size=3, stride=2, padding=1, output_padding=1)
 
         if self.use_batch_norm:
             self.transposed_conv_layer_1 = nn.Sequential(transposed_conv_1, nn.BatchNorm2d(self.hidden_dimensions[4]), self.activation_function())
@@ -93,7 +93,7 @@ class LargeFilterSizesMaxPoolVAE(BaseVAE):
 def main():
     from torchinfo import summary
 
-    model = LargeFilterSizesMaxPoolVAE({
+    model = SmallFilterSizesMaxPoolVAE({
         "input_channels": 3,
         "latent_size": 32,
         "hidden_dimensions": [8, 16, 32, 64, 128, 256],
