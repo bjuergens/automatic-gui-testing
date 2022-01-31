f6749a9f8127ed97cca82f1861d0dce5fc52d597
Patrick Deubel <patrick.deubel@gmail.com>
2022-01-31 16:06:09 +0100

    Fixed ImprovedSummaryWriter getting the latest version

--- utils/logging/improved_summary_writer.py
+++ utils/logging/improved_summary_writer.py
@@ -16,13 +16,10 @@ class ImprovedSummaryWriter(SummaryWriter):
         if not os.path.exists(root_save_dir):
             os.makedirs(root_save_dir)
 
-        root_dir_content = [sub_dir for sub_dir in os.listdir(root_save_dir) if "version" in sub_dir]
+        root_dir_content = [int(sub_dir.split("version_")[-1]) for sub_dir in os.listdir(root_save_dir) if "version" in sub_dir]
         root_dir_content.sort()
 
-        if len(root_dir_content) > 0:
-            self.version_number = int(root_dir_content[-1].split("version_")[-1]) + 1
-        else:
-            self.version_number = 0
+        self.version_number = 0 if len(root_dir_content) == 0 else root_dir_content[-1] + 1
 
         save_dir = os.path.join(root_save_dir, f"version_{self.version_number}")
         os.makedirs(save_dir)
