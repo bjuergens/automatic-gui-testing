MDN Prediction: Fixed a bug where a wrong index was selected due to numerical instability
- On prediction a categorical distribution is created to select which gaussian mixture is used per latent dimension
or overall. There is a very rare case where the pi's (mixture weights) did not sum to 1 but to 0.9999994 or sth similar
small. When then 0.9999995 or higher was drawn for the categorical distribution sample it could lead to an index
out of bounds error in the mus and sigmas. This is fixed now by making the last entry of the cumulated sum of the
mixture weights 1.0
- Additionally refactored the forward and predict functions for the GMM used in both the StandardMDN and MDNBCE as there
is no reason to have the implementation twice

models/rnn/base_rnn.py
@@ -113,7 +113,7 @@ class BaseMDNRNN(BaseRNN):
         self.number_of_gaussians = model_parameters["number_of_gaussians"]
         self.use_gaussian_per_latent_dim: bool = model_parameters["use_gaussian_per_latent_dim"]
 
-    def predict(self, model_output, latents, temperature):
+    def _predict_gaussian_mixture(self, model_output, temperature):
         # Temperature parameter is used at two points and only during sampling (not during training):
         # log_pi is divided with the temperature and sigma is multiplied with the square root of the temperature.
         # This is done in the reference implementation of the world model approach, specifically here:
@@ -127,7 +127,6 @@ class BaseMDNRNN(BaseRNN):
         mus = model_output[0]
         sigmas = model_output[1]
         log_pi = model_output[2]
-        rewards = model_output[3]
 
         number_of_gaussians = mus.size(2)
         latent_size = mus.size(3)
@@ -150,43 +149,53 @@ class BaseMDNRNN(BaseRNN):
         # Randomly select a gaussian distribution either per dimension of the latent vector
         # (self.use_gaussian_per_latent_dim == True) or one gaussian distribution in general if
         # (self.use_gaussian_per_latent_dim == False)
+
+        cumulated_sum = pi_temperature_adjusted.cumsum(dim=1)
+        # Last entry is already 1.0 as the pis sum up to 1.0 because of the calculation above, but sometimes there
+        # are slight rounding errors leading to results like 1.000001 or worse 0.9999994 etc. This leads to errors
+        # in the drawing of the categorical distribution below, because if the random number is then 0.9999995
+        # it would lead to a list of all False's which leads count_nonzero to count 0, which leads to getting an
+        # index of num_gaussians - 0 = num_gaussians and this would be out of bounds!
+        cumulated_sum[:, -1] = 1.0
+
         if self.use_gaussian_per_latent_dim:
             pi_count = torch.count_nonzero(
-                pi_temperature_adjusted.cumsum(dim=1)
-                >= torch.rand((pi_temperature_adjusted.size(0), 1), device=self.device),
+                cumulated_sum
+                >= torch.rand((latent_size, 1), device=self.device),
                 dim=1
             )
-            drawn_mixtures = pi_temperature_adjusted.size(1) - pi_count
         else:
             # Take the cumulated sum of the pi's and then use a random number of the uniform distribution to do create
             # a categorical distribution. The first pi in the cumulated sum that is larger than the random number is
             # the sample drawn. To get the first number simply count all non zeros (i.e. larger pi's) and then subtract
             # from the size of pi at dim 1 (i.e. number_of_gaussians). Hopefully this is faster than using
             # torch.distributions.Categorical
-            pi_count = torch.count_nonzero(pi_temperature_adjusted.cumsum(dim=1) >= torch.rand(1, device=self.device))
-            drawn_mixtures = pi_temperature_adjusted.size(1) - pi_count
+            pi_count = torch.count_nonzero(cumulated_sum >= torch.rand(1, device=self.device))
+
+        drawn_mixtures = number_of_gaussians - pi_count
 
         # Shape after this for selected_mus and selected_sigmas: (1, 1, L_SIZE)
         if self.use_gaussian_per_latent_dim:
-            selected_mus = torch.gather(mus, dim=1, index=drawn_mixtures.unsqueeze(-1)).view(latents.size())
-            selected_sigmas = torch.gather(sigmas, dim=1, index=drawn_mixtures.unsqueeze(-1)).view(latents.size())
+            selected_mus = torch.gather(mus, dim=1, index=drawn_mixtures.unsqueeze(-1)).view(1, 1, latent_size)
+            selected_sigmas = torch.gather(sigmas, dim=1, index=drawn_mixtures.unsqueeze(-1)).view(1, 1, latent_size)
         else:
-            selected_mus = mus[:, drawn_mixtures].view(latents.size())
-            selected_sigmas = sigmas[:, drawn_mixtures].view(latents.size())
+            selected_mus = mus[:, drawn_mixtures].view(1, 1, latent_size)
+            selected_sigmas = sigmas[:, drawn_mixtures].view(1, 1, latent_size)
 
         # Now use the randomly selected gaussian(s) to sample the next latent vector, i.e. the prediction
-        random_vector = torch.randn(size=latents.size(), device=self.device)
+        random_vector = torch.randn(size=(1, 1, latent_size), device=self.device)
         latent_prediction = selected_mus + random_vector * selected_sigmas * torch.sqrt(temperature)
+
+        return latent_prediction
+
+    def predict(self, model_output, latents, temperature):
+        latent_prediction = self._predict_gaussian_mixture(model_output, temperature)
+        rewards = model_output[3]
 
         # latent_prediction: (BATCH_SIZE, SEQ_LEN, L_SIZE)
         return latent_prediction, self.denormalize_reward(rewards)
 
-    def forward(self, latents: torch.Tensor, actions: torch.Tensor):
-        sequence_length = latents.size(1)
-
-        outputs, _ = self.rnn_forward(latents, actions)
-        gmm_outputs = self.fc(outputs)
-
+    def _forward_gaussian_mixture(self, gmm_outputs, sequence_length):
         stride = self.number_of_gaussians * self.latent_size
 
         if self.use_gaussian_per_latent_dim:
@@ -215,6 +224,16 @@ class BaseMDNRNN(BaseRNN):
         # The pi's shall sum to one, therefore take the softmax over dimension 2 (number_of_gaussians)
         # Use log_softmax for numerical stability as we also compute NLLLoss directly in log-space
         log_pi = f.log_softmax(pi, dim=2)
+
+        return mus, sigmas, log_pi, rewards
+
+    def forward(self, latents: torch.Tensor, actions: torch.Tensor):
+        sequence_length = latents.size(1)
+
+        outputs, _ = self.rnn_forward(latents, actions)
+        gmm_outputs = self.fc(outputs)
+
+        mus, sigmas, log_pi, rewards = self._forward_gaussian_mixture(gmm_outputs, sequence_length)
 
         rewards = self.reward_output_activation_function(rewards)
 

models/rnn/mdn_rnn.py
@@ -33,69 +33,8 @@ class MDNRNNWithBCE(StandardMDNRNN):
         self.denormalize_reward = lambda x: torch.round(x).int()
 
     def predict(self, model_output, latents, temperature):
-        # Temperature parameter is used at two points and only during sampling (not during training):
-        # log_pi is divided with the temperature and sigma is multiplied with the square root of the temperature.
-        # This is done in the reference implementation of the world model approach, specifically here:
-        # https://github.com/hardmaru/WorldModelsExperiments/blob/master/doomrnn/doomrnn.py#L625 and on line 639
-
-        # Shapes:
-        # latents: (1, 1, L_SIZE)
-        # mus, sigmas: (1, 1, N_GAUSS, L_SIZE)
-        # log_pi: (1, 1, N_GAUSS, 1) or (1, 1, N_GAUSS, L_SIZE) depending on
-        #         self.use_gaussian_per_latent_dim
-        mus = model_output[0]
-        sigmas = model_output[1]
-        log_pi = model_output[2]
+        latent_prediction = self._predict_gaussian_mixture(model_output, temperature)
         predicted_reward_in_logits = model_output[3]
-
-        number_of_gaussians = mus.size(2)
-        latent_size = mus.size(3)
-
-        # Remove (1, 1) dimensions and transpose to get (L_SIZE, N_GAUSS) dimensions
-        mus = mus.view(number_of_gaussians, latent_size).t()
-        sigmas = sigmas.view(number_of_gaussians, latent_size).t()
-
-        if self.use_gaussian_per_latent_dim:
-            log_pi = log_pi.view(number_of_gaussians, latent_size).t()
-        else:
-            log_pi = log_pi.view(number_of_gaussians, 1).t()
-
-        # Exp-Normalization trick to calculate the softmax, useful to ensure numerical stability
-        log_pi_temperature_adjusted = (log_pi / temperature)
-        log_pi_temperature_adjusted -= log_pi_temperature_adjusted.max()
-        log_pi_temperature_adjusted = log_pi_temperature_adjusted.exp()
-        pi_temperature_adjusted = log_pi_temperature_adjusted / log_pi_temperature_adjusted.sum(dim=1, keepdim=True)
-
-        # Randomly select a gaussian distribution either per dimension of the latent vector
-        # (self.use_gaussian_per_latent_dim == True) or one gaussian distribution in general if
-        # (self.use_gaussian_per_latent_dim == False)
-        if self.use_gaussian_per_latent_dim:
-            pi_count = torch.count_nonzero(
-                pi_temperature_adjusted.cumsum(dim=1)
-                >= torch.rand((pi_temperature_adjusted.size(0), 1), device=self.device),
-                dim=1
-            )
-            drawn_mixtures = pi_temperature_adjusted.size(1) - pi_count
-        else:
-            # Take the cumulated sum of the pi's and then use a random number of the uniform distribution to do create
-            # a categorical distribution. The first pi in the cumulated sum that is larger than the random number is
-            # the sample drawn. To get the first number simply count all non zeros (i.e. larger pi's) and then subtract
-            # from the size of pi at dim 1 (i.e. number_of_gaussians). Hopefully this is faster than using
-            # torch.distributions.Categorical
-            pi_count = torch.count_nonzero(pi_temperature_adjusted.cumsum(dim=1) >= torch.rand(1, device=self.device))
-            drawn_mixtures = pi_temperature_adjusted.size(1) - pi_count
-
-        # Shape after this for selected_mus and selected_sigmas: (1, 1, L_SIZE)
-        if self.use_gaussian_per_latent_dim:
-            selected_mus = torch.gather(mus, dim=1, index=drawn_mixtures.unsqueeze(-1)).view(latents.size())
-            selected_sigmas = torch.gather(sigmas, dim=1, index=drawn_mixtures.unsqueeze(-1)).view(latents.size())
-        else:
-            selected_mus = mus[:, drawn_mixtures].view(latents.size())
-            selected_sigmas = sigmas[:, drawn_mixtures].view(latents.size())
-
-        # Now use the randomly selected gaussian(s) to sample the next latent vector, i.e. the prediction
-        random_vector = torch.randn(size=latents.size(), device=self.device)
-        latent_prediction = selected_mus + random_vector * selected_sigmas * torch.sqrt(temperature)
 
         # latent_prediction: (BATCH_SIZE, SEQ_LEN, L_SIZE)
         return latent_prediction, self.denormalize_reward(torch.sigmoid(predicted_reward_in_logits))
@@ -106,36 +45,9 @@ class MDNRNNWithBCE(StandardMDNRNN):
         outputs, _ = self.rnn_forward(latents, actions)
         gmm_outputs = self.fc(outputs)
 
-        stride = self.number_of_gaussians * self.latent_size
-
-        # Don't apply activation function on rewards as the BCELossWithLogits applies a sigmoid internally.
-        # Numerically more stable this way
-        if self.use_gaussian_per_latent_dim:
-            mus, sigmas, pi, rewards = torch.split(
-                gmm_outputs,
-                split_size_or_sections=(stride, stride, stride, 1),
-                dim=-1
-            )
-        else:
-            mus, sigmas, pi, rewards = torch.split(
-                gmm_outputs,
-                split_size_or_sections=(stride, stride, self.number_of_gaussians, 1),
-                dim=-1
-            )
-
-        mus = mus.view(self.batch_size, sequence_length, self.number_of_gaussians, self.latent_size)
-
-        sigmas = sigmas.view(self.batch_size, sequence_length, self.number_of_gaussians, self.latent_size)
-        sigmas = torch.exp(sigmas)
-
-        if self.use_gaussian_per_latent_dim:
-            pi = pi.view(self.batch_size, sequence_length, self.number_of_gaussians, self.latent_size)
-        else:
-            pi = pi.view(self.batch_size, sequence_length, self.number_of_gaussians, 1)
-
-        # The pi's shall sum to one, therefore take the softmax over dimension 2 (number_of_gaussians)
-        # Use log_softmax for numerical stability as we also compute NLLLoss directly in log-space
-        log_pi = f.log_softmax(pi, dim=2)
+        # Difference to StandardMDN: No activation function on reward, as the sigmoid applied on it is included in the
+        # BCELoss in the Loss Function for numerical stability
+        mus, sigmas, log_pi, rewards = self._forward_gaussian_mixture(gmm_outputs, sequence_length)
 
         return mus, sigmas, log_pi, rewards
 