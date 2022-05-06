41e6306d2bad716407fd9fc11cbb26311b927c3c
bjoern <git@wotanii.de>
2022-05-06 13:15:03 +0200

    e2e script now can train m-model

--- full_e2e.py
+++ full_e2e.py
@@ -17,9 +17,9 @@ logger.setLevel(logging.INFO)
 @click.command()
 @click.option("--gen-seq-len", "generator_sequence_length", type=int, default=5,
               help="generate ground_truth with actions sequences of this length")
-@click.option("--gen-seq-no", "generator_sequence_number", type=int, default=2,
+@click.option("--gen-seq-no", "generator_sequence_number", type=int, default=16,
               help="generate ground_truth with total number of actions sequences")
-@click.option("--gen-work-no", "generator_worker_number", type=int, default=2,
+@click.option("--gen-work-no", "generator_worker_number", type=int, default=16,
               help="generate ground_truth with this number of paralell worker process. Recommended: Twice the CPU cores")
 @click.option("--gen-monkey-type", "generator_monkey_type", type=str, default="random-clicks",
               help="generate ground_truth with this type of monkey-tester")
@@ -27,9 +27,9 @@ logger.setLevel(logging.INFO)
               help="root-dir for experiments")
 @click.option("--config-v", "orig_config_v", type=str, default="_e2e_cfg/2_vae_config.yaml",
               help="Path to a YAML configuration containing training options for V-Model")
-@click.option("--config-m", "orig_config_m", type=str,
+@click.option("--config-m", "orig_config_m", type=str, default="_e2e_cfg/3_mdn_rnn_config.yaml",
               help="Path to a YAML configuration containing training options for M-Model")
-@click.option("--config-c", "orig_config_c", type=str,
+@click.option("--config-c", "orig_config_c", type=str, default="_e2e_cfg/4_controller_config.yaml",
               help="Path to a YAML configuration containing training options for C-Model")
 @click.option("--comet/--no-comet", type=bool, default=False,
               help="Disable logging to Comet (automatically disabled when API key is not provided in home folder)")
@@ -43,21 +43,130 @@ def main(orig_config_v: str, orig_config_m: str, orig_config_c: str, comet: bool
     work_env = os.environ.copy()
     work_env['PYSIDE_DESIGNER_PLUGINS'] = '.'  # https://stackoverflow.com/a/68605424/5132456
     logging.info(f"work_dir: {base_dir}")
-    Path(base_dir).mkdir(parents=True, exist_ok=True)
 
     work_dir_generator = Path(base_dir).joinpath("01_generator")
+    work_dir_vae = Path(base_dir).joinpath("02_vae")
+    work_dir_rnn = Path(base_dir).joinpath("03_rnn")
+
+    Path(work_dir_generator).mkdir(parents=True, exist_ok=True)
+    Path(work_dir_vae).mkdir(parents=True, exist_ok=True)
+    Path(work_dir_rnn).mkdir(parents=True, exist_ok=True)
+
     generate(work_dir_generator, work_env, generator_sequence_number, generator_worker_number,
              generator_sequence_length)
-    work_config_v = os.path.join(base_dir, "config_v.yaml")
+
+    vae_data_path = os.path.join(work_dir_vae, "data")
+    rnn_data_path = os.path.join(work_dir_rnn, "data")
+    log_dir = os.path.join(base_dir, "log")
+    # prepare_vae_data(work_dir_generator, work_env, vae_data_path)
+    # train_vae(work_dir_vae, work_env, orig_config_v, vae_data_path, log_dir)
+    prepare_rnn_data(work_dir_generator, vae_data_path, work_env, rnn_data_path, generator_sequence_length)
+    train_rnn(work_dir_rnn, work_env, orig_config_m, rnn_data_path, log_dir)
+
+
+def prepare_rnn_data(work_dir_generator, vae_data_path, work_env, target_path, sequence_length, skip_of_exists=False):
+    dirpath = Path(target_path)
+    if dirpath.exists() and dirpath.is_dir():
+        if skip_of_exists:
+            logging.info(f"skipping because target dir {dirpath} already exists ")
+            return
+        logging.info(f"cleaning up old datapath {dirpath}")
+        shutil.rmtree(dirpath)
+
+    target_train = Path(target_path) / "train" / str(sequence_length)
+    target_val = Path(target_path) / "val" / str(sequence_length)
+    target_test = Path(target_path) / "test" / str(sequence_length)
+
+    Path(target_train).parent.mkdir(parents=True, exist_ok=True)
+    Path(target_val).parent.mkdir(parents=True, exist_ok=True)
+    Path(target_test).parent.mkdir(parents=True, exist_ok=True)
+
+    for generator_type in os.listdir(work_dir_generator):
+        gen_type_path = os.path.join(work_dir_generator, generator_type)
+        for experiment_date in os.listdir(gen_type_path):
+            exp_path = Path(gen_type_path) / experiment_date
+            logging.info(f"found generator_run in {exp_path}")
+            logging.info(f"use everything for training at {target_train}")
+            shutil.copytree(exp_path, target_train)
+
+            logging.info(f"use first for val at {target_val}")
+            shutil.copytree(exp_path / "0", target_val / "0")
+
+            logging.info(f"use second for test at {target_test}")
+            shutil.copytree(exp_path / "1", target_test / "1")
+
+
+def train_rnn(work_dir, work_env, orig_config, rnn_data_path, log_dir):
+    work_config = os.path.join(work_dir, "config_m.yaml")
+    shutil.copyfile(orig_config, work_config)
+    replace_in_yaml(work_config, work_config,
+                    key_path=['experiment_parameters', 'dataset_path'],
+                    value=rnn_data_path)
+    replace_in_yaml(work_config, work_config,
+                    key_path=['logging_parameters', 'save_dir'],
+                    value=log_dir)
+    args_v_train = ["python", "train_mdn_rnn.py",
+                    "-c", work_config,
+                    "--disable-comet"]
+    run_script(args_v_train, work_env)
+
+
+def prepare_vae_data(work_dir_generator, work_env, target_path, skip_of_exists=True):
+    dirpath = Path(target_path)
+    if dirpath.exists() and dirpath.is_dir():
+        if skip_of_exists:
+            logging.info(f"skipping because target dir {dirpath} already exists ")
+            return
+        logging.info(f"cleaning up old datapath {dirpath}")
+        shutil.rmtree(dirpath)
+    for generator_type in os.listdir(work_dir_generator):
+        gen_type_path = os.path.join(work_dir_generator, generator_type)
+        for experiment_date in os.listdir(gen_type_path):
+            exp_path = os.path.join(gen_type_path, experiment_date)
+            mixed_path = exp_path + "-mixed"
+            dedup_path = mixed_path + "-deduplicated-images"
+            split_path = dedup_path + "-splits"
+
+            logging.info(f"found generator_run in {exp_path}")
+
+            args_copy = ["python", "data/data_processing/copy_images.py",
+                         "-d", exp_path]
+            run_script(args_copy, work_env)
+            logging.info(f"total files in mixed: {len(os.listdir(mixed_path))}")
+
+            args_dedup = ["python", "data/data_processing/remove_duplicate_images.py",
+                          "-d", mixed_path]
+            run_script(args_dedup, work_env)
+            dedup_file_count = len(os.listdir(dedup_path))
+            if dedup_file_count < 10:
+                logging.warning("too few files left after deduplication. Expect problems in later steps.")
+            logging.info(f"total files in dedup: {dedup_file_count}")
+            args_dedup = ["python", "data/data_processing/create_dataset_splits.py",
+                          "-d", dedup_path]
+            run_script(args_dedup, work_env)
+            logging.info(f"total files in split: {len(os.listdir(split_path))}")
+
+            shutil.move(split_path, target_path)
+
+            logging.info(f"cleanup intermediate files: {mixed_path}")
+            shutil.rmtree(mixed_path)
+            logging.info(f"cleanup intermediate files: {dedup_path}")
+            shutil.rmtree(dedup_path)
+
+
+def train_vae(work_dir, work_env, orig_config_v, vae_data_path, log_dir):
+    work_config_v = os.path.join(work_dir, "config_v.yaml")
     shutil.copyfile(orig_config_v, work_config_v)
-
     replace_in_yaml(work_config_v, work_config_v,
-                    key_path=['experiment_parameters'],
-                    value={"aa": 123, "bb": 324})
-
+                    key_path=['experiment_parameters', 'dataset_path'],
+                    value=vae_data_path)
     replace_in_yaml(work_config_v, work_config_v,
                     key_path=['logging_parameters', 'save_dir'],
-                    value='_e2e_/01_generator/random-clicks/2022-05-01_12-23-34-mixed-deduplicated-images-splits')
+                    value=log_dir)
+    args_v_train = ["python", "train_vae.py",
+                    "-c", work_config_v,
+                    "--disable-comet"]
+    run_script(args_v_train, work_env)
 
 
 def generate(work_dir, work_env, generator_sequence_number, generator_worker_number, generator_sequence_length):
